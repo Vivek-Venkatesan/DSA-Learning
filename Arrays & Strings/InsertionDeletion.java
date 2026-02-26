b169739e457ea4855c8cc9d8653afe54d3ef57d1
vivek.venkatesan <vivek.venkatesan@impacteers.com>
2026-02-26 11:06:24 +0530

    insert and delete

--- Arrays & Strings/InsertionDeletion.java
+++ Arrays & Strings/InsertionDeletion.java
@@ -1,3 +1,38 @@
 public class InsertionDeletion {
-    
+
+    //1. Insert particular element into a specific index into the Array
+    public void insert(int[] arr, int size, int index, int value) {
+        for(int i = size; i > index; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[index] = value;
+        
+        /*
+        We start from the last since we dont want to overwrite the values stored in the indexes upfront since Arrays are fixed sized collection entity
+        Adding or inserting from the last makes sense since the existing data would not be disturbed
+        */
+    }
+    //2. Delete a particular element from the Array
+   public void delete(int[] arr, int size, int index) {
+        for (int i = index; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+    }
+
+    //3. Find if there exists pair with sum = target in SORTED array. Input: [1,2,3,4,5] Target = 6
+    public boolean hasPair(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) return true;
+            else if (sum < target) left++;
+            else right--;
+        }
+
+        return false;
+    }
+
 }
--- Arrays & Strings/Main.java
+++ Arrays & Strings/Main.java
@@ -4,11 +4,18 @@ public class Main {
         System.out.println("Hello");
          int[] arr = {1, 2, 3, 4, 5};
         Basics arrayBasics = new Basics();
+        InsertionDeletion arrayInserts = new InsertionDeletion();
         arrayBasics.sumOfAllElements(arr);
         arrayBasics.indexBasedTraversal(arr);
         arrayBasics.enhancedForLoop(arr);
         arrayBasics.reverseTraversal(arr);
         arrayBasics.reverse(arr);
         arrayBasics.isSorted(arr);
+        int size = 5;
+        int index = 3;
+        int value = 9;
+        arrayInserts.insert(arr, size, index, value);
+        arrayInserts.delete(arr, size, index);
+        arrayInserts.hasPair(arr, value);
     }
 }

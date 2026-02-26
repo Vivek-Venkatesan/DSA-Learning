6de8a37937d341e5d540f6e498576d948d9d3687
vivek.venkatesan <vivek.venkatesan@impacteers.com>
2026-02-26 10:05:55 +0530

    array basics

---
+++ Arrays & Strings/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+         int[] arr = {1, 2, 3, 4, 5};
+        ArrayBasics arrayBasics = new ArrayBasics();
+        arrayBasics.sumOfAllElements(arr);
+        arrayBasics.indexBasedTraversal(arr);
+    }
+}
